775eb114736b6fe456f3a9f966c9645f65816780
Minchuk Olga <olga@MacBook-Pro-Olga.local>
2021-05-20 12:13:36 +0300

    Updates

---
+++ precache-manifest.bf86c4acb5f892175e548bb195c538e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf23ed7c6d2c95c6c28d28aafb95241d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13caf77f7e1d96a2c4a8",
+    "url": "./static/css/main.30b141ad.chunk.css"
+  },
+  {
+    "revision": "f605fb10a39f70f0b262",
+    "url": "./static/js/2.c9d11b7c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c9d11b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13caf77f7e1d96a2c4a8",
+    "url": "./static/js/main.291a614e.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.bf86c4acb5f892175e548bb195c538e8.js"
 );
 
 self.addEventListener('message', (event) => {
